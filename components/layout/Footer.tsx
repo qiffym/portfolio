4ce4b1680f586d05b22ec43a0a8e61c64b9975a9
Qiff Ya Muhammad <qiww.dev@gmail.com>
2023-05-13 10:45:27 +0700

    npm update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,20 +1,19 @@
-import Link from 'next/link';
 import React from 'react';
 
 let year = new Date().getFullYear();
 
 const Footer = () => {
-  return (
-    <footer className="footer footer-center bg-neutral p-10 text-neutral-content">
-      <div>
-        <small className="text-slate-300">
-          <span>Design with ❤️ by. My Self</span> <br />
-          <span>Web Developement Enthusiast</span>
-        </small>
-        <p>Copyright &copy; {year} - All right reserved</p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="footer footer-center bg-neutral p-10 text-neutral-content">
+            <div>
+                <small className="text-slate-300">
+                    <span>Design with ❤️ by. My Self</span> <br />
+                    <span>Web Developement Enthusiast</span>
+                </small>
+                <p>Copyright &copy; {year} - All right reserved</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
